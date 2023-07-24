7f75c472388742d13863d854c4d1021080605e77
xavierVinas <xavivinass@gmail.com>
2023-07-24 12:29:40 +0200

    first commit

---
+++ pages/api/hello.js
@@ -0,0 +1,9 @@
+export default function handler(req, res) {
+    res.status(200).json({ text: 'Hello' });
+}
+
+
+// export default function handler(req, res) {
+//     const email = req.body.email;
+//     // Then save email to your database, etc...
+// }
